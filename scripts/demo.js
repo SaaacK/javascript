a7a99c6cf6b7fdaaf6154231c02e403663fcc313
Franco Zambelli Castillo <fzambelli@contempora.local>
2020-02-06 16:49:23 -0300

    commit

---
+++ scripts/demo.js
@@ -0,0 +1,41 @@
+
+//muestra
+alert("This is an alert box!");
+var x="Hola mundo";
+
+//asignacion de variables
+var num1=8;
+var num2=55.55;
+var sayHello = 'Hello world! \'I am a JavaScript programmer.\' ';
+
+//booleanos
+var isActive = true; 
+var isHoliday = false;
+
+//operadores aricmeticos
+
+var y=2+2;
+
+var z=46+y+y;
+
+//operador ternario
+var ageyoung = 15;
+var ageold = 25;
+var isAdult = (ageyoung < 18) ? "Too young": "Old enough";
+
+
+//condicionales
+var myNum1 = 7;
+var myNum2 = 10;
+
+
+var num3 = 5;
+var num4 = 20;
+
+//condicional if else
+var ifelse1 = 24;
+var ifelse2 = 7;
+
+
+
+
